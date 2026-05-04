01ec5e37a32767ba869f890f24a3f036771340da
Elias Engström <elias.engstrom@elev.ntig.se>
2026-05-04 13:41:27 +0200

    Börjat med Books classen

--- Projekt-2/library/src/main/java/com/ee/Books.java
+++ Projekt-2/library/src/main/java/com/ee/Books.java
@@ -1,5 +1,28 @@
 package com.ee;
 
-public class Books {
+// Klass Books, ärver av Publications, representerar en bok.
 
+public class Books extends Publications {
+    public String author;
+    public String genre;
+    public int pages;
+
+    public Books(String id, String title, boolean isAvalible, String auther, String genre, int pages, String author) {
+        super(id, title, isAvalible);
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+    }
+
+    @Override
+    public void printInfo() {
+        IO.println("---------------Books------------");
+        IO.println("ID         : " + id);
+        IO.println("Titel      : " + title);
+        IO.println("Författare : " + author);
+        IO.println("Genre      : " + genre);
+        IO.println("Sidor      : " + pages);
+        IO.println("Tillgänglig: " + (isAvailable ? "Ja" : "Nej"));
+        IO.println("---------------------------");
+    }
 }
