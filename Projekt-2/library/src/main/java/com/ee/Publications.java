a474357e86c664a4b23d31972838a1dc8ed8efd1
Elias Engström <elias.engstrom@elev.ntig.se>
2026-05-06 10:20:22 +0200

    Tillfälligt klar med publications klassen

--- Projekt-2/library/src/main/java/com/ee/Publications.java
+++ Projekt-2/library/src/main/java/com/ee/Publications.java
@@ -1,5 +1,25 @@
 package com.ee;
 
-public class Publications {
+// En basklass Publications som innehåller gemensamma variabler för books och magazines
 
+public abstract class Publications {
+    public String id;
+    public String title;
+    public boolean isAvailable;
+
+    public Publications(String id, String title, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public abstract void printInfo();
 }
