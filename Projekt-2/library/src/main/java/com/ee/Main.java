41ae4a31146da216c027d74065fca783c8e8acb9
Elias Engström <elias.engstrom@elev.ntig.se>
2026-05-13 10:41:02 +0200

    main är typ klar

--- Projekt-2/library/src/main/java/com/ee/Main.java
+++ Projekt-2/library/src/main/java/com/ee/Main.java
@@ -1,6 +1,6 @@
 package com.ee;
 
-import java.util.ArrayList;;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -23,7 +23,7 @@ public class Main {
             IO.println("5. Lägg till tidningar manuellt");
             IO.println("6. Avsluta");
             IO.println("==========================");
-            IO.println("Ditt val: ");
+            IO.print("Ditt val: ");
 
             val = scanner.nextInt();
             scanner.nextLine();
@@ -32,12 +32,12 @@ public class Main {
                 IO.println("Hämtar böcker...");
                 ArrayList<Books> hämtade = client.fetchBooks();
                 books.addAll(hämtade);
-                IO.println(hämtade.size() + "böcker hämtade och sparade.");
+                IO.println(hämtade.size() + " böcker hämtade och sparade.");
             } else if (val == 2) {
                 IO.println("Hämtar tidningar...");
                 ArrayList<Magazines> hämtade = client.fetchMagazines();
                 magazines.addAll(hämtade);
-                IO.println(hämtade.size() + "tidningar hämtade och sparade.");
+                IO.println(hämtade.size() + " tidningar hämtade och sparade.");
             } else if (val == 3) {
                 IO.println("Vad vill du skriva ut?");
                 IO.println("1. Böcker");
@@ -66,9 +66,53 @@ public class Main {
                     }
                 }
             } else if (val == 4) {
+                IO.println("\n--- Lägg till ny bok ---");
 
+                String id = String.valueOf(books.size() + 1);
+
+                IO.print("Titel: ");
+                String title = scanner.nextLine();
+
+                IO.print("Författare: ");
+                String author = scanner.nextLine();
+
+                IO.print("Genre: ");
+                String genre = scanner.nextLine();
+
+                IO.print("Antal sidor: ");
+                int pages = scanner.nextInt();
+                scanner.nextLine();
+
+                books.add(new Books(id, title, true, author, genre, pages, author));
+                IO.println("Boken \"" + title + "\" har lagts till.");
+            } else if (val == 5) {
+                IO.println("\n --- Lägg till ny tidning ---");
+
+                String id = String.valueOf(magazines.size() + 1);
+
+                System.out.print("Titel: ");
+                String title = scanner.nextLine();
+
+                System.out.print("Nummer (issueNumber): ");
+                int issueNumber = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Kategori: ");
+                String category = scanner.nextLine();
+
+                System.out.print("Utgivningsår: ");
+                int publishedYear = scanner.nextInt();
+                scanner.nextLine();
+
+                magazines.add(new Magazines(id, title, false, issueNumber, category, publishedYear));
+                IO.println("Tidningen \"" + title + "\" har lagts till.");
+            } else if (val == 6) {
+                IO.println("Avslutar Bibliotekssystemet. Hej då!");
+            } else {
+                IO.println("Ogiltigt val, försök igen.");
             }
 
         }
+        scanner.close();
     }
 }
